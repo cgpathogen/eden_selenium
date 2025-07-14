ea7de754617d407ac38a3ce0044178b62474f23d
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-14 18:20:02 +0300

    создан экземпляр класса credentials для работы с персональными данными

--- base/base_test.py
+++ base/base_test.py
@@ -1,6 +1,9 @@
 from pages.main_page.main_page import MainPage
 
+from data.credentials.credentials import Credentials
+
 class BaseTest:
 
     def setup_method(self):
         self.mainPage = MainPage(self.driver)
+        self.credentials = Credentials()
