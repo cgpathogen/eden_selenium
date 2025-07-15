e2e06c85c1cbc8e90db8b372c9afb5033fb7e376
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-15 16:11:52 +0300

    Удалён лишний файл

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -20,13 +20,3 @@ class TestAuthorization(BaseTest):
         self.mainPage.click_enter_button()
         self.mainPage.get_popup_title()
         time.sleep(1.5)
-
-
-    @allure.title("test chosen city is equal to the city id dropdown")
-    def test_chosen_city_align(self):
-        self.mainPage.open()
-        self.mainPage.click_choose_no_button()
-        self.mainPage.enter_text_to_search_city_input(self.mainPage.search_city_name)
-        self.mainPage.choose_city_from_result()
-        self.mainPage.check_city_name_align()
-        time.sleep(1.5)
