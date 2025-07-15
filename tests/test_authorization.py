59bfd9c5f2d06046cd76807c329079f64f50b0c1
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-15 10:06:06 +0300

    Добавлен тест для проверки совпадения названия выбранного города в поп-апе и дропдауне

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,9 +1,18 @@
+import time
 import allure
 from base.base_test import BaseTest
+from conftest import driver
+
 
 @allure.epic("User")
 @allure.story("Authorization")
 class TestAuthorization(BaseTest):
 
-    def test_authorization_as_registered_user(self):
+    @allure.title("test chosen city is equal to the city id dropdown")
+    def test_chosen_city_align(self):
         self.mainPage.open()
+        self.mainPage.click_choose_no_button()
+        self.mainPage.enter_text_to_search_city_input(self.mainPage.search_city_name)
+        self.mainPage.choose_city_from_result()
+        self.mainPage.check_city_name_align()
+        time.sleep(1.5)
