58a6c33b4dadb10c2c676e2564bb0249e38e00ef
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-15 11:47:04 +0300

    Добавлен тест на лог ин/лог аут

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,12 +1,26 @@
 import time
 import allure
 from base.base_test import BaseTest
-from conftest import driver
 
 
 @allure.epic("User")
 @allure.story("Authorization")
 class TestAuthorization(BaseTest):
+
+    @allure.title("test authorization as registered user")
+    def test_login_logout_as_registered_user(self):
+        self.mainPage.open()
+        self.mainPage.click_enter_button()
+        self.mainPage.auth_enter_email(self.credentials.login)
+        self.mainPage.auth_enter_password(self.credentials.password)
+        self.mainPage.auth_click_enter_button()
+        self.mainPage.click_enter_button()
+        assert self.credentials.login == self.personalCabinetPage.get_username_text()
+        self.personalCabinetPage.nav_links.click_logout_link()
+        self.mainPage.click_enter_button()
+        self.mainPage.get_popup_title()
+        time.sleep(1.5)
+
 
     @allure.title("test chosen city is equal to the city id dropdown")
     def test_chosen_city_align(self):
