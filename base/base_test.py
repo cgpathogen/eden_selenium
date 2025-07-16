9e9d2d64b332ea50c86a84841b01e01cb87a5596
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-16 10:54:22 +0300

    добавлен экземпляр страницы каталога

--- base/base_test.py
+++ base/base_test.py
@@ -1,5 +1,6 @@
 from pages.main_page.main_page import MainPage
 from pages.personal_cabinet_page.personal_cabinet_page import PersonalCabinetPage
+from pages.catalogue_page.catalogue_page import CataloguePage
 
 from data.credentials.credentials import Credentials
 
@@ -8,4 +9,5 @@ class BaseTest:
     def setup_method(self):
         self.mainPage = MainPage(self.driver)
         self.personalCabinetPage = PersonalCabinetPage(self.driver)
+        self.cataloguePage = CataloguePage(self.driver)
         self.credentials = Credentials()
