662fb89dc7fc852bb0335781a4c4f4d5aa6b280a
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-18 12:59:59 +0300

    Создан экземпляр класса для страницы оформления заказа

--- base/base_test.py
+++ base/base_test.py
@@ -2,6 +2,7 @@ from pages.main_page.main_page import MainPage
 from pages.personal_cabinet_page.personal_cabinet_page import PersonalCabinetPage
 from pages.catalogue_page.catalogue_page import CataloguePage
 from pages.cart_page.cart_page import CartPage
+from pages.order_page.order_page import OrderPage
 
 from data.credentials.credentials import Credentials
 
@@ -12,4 +13,5 @@ class BaseTest:
         self.personalCabinetPage = PersonalCabinetPage(self.driver)
         self.cataloguePage = CataloguePage(self.driver)
         self.cartPage = CartPage(self.driver)
+        self.orderPage = OrderPage(self.driver)
         self.credentials = Credentials()
