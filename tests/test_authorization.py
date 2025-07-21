9e27cd580c7c0f972e7b5b3ca0fcad2a27e1dab0
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-21 11:40:01 +0300

    Добавлены тесты на: - авторизацию без логина - авторизацию с неверным паролем - авторизацию с паролем, буквы в котором в одного регистре - авторизацию с незаполненными полями

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -20,3 +20,44 @@ class TestAuthorization(BaseTest):
         self.mainPage.click_enter_button()
         self.mainPage.get_popup_title()
         time.sleep(1.5)
+
+
+    @allure.title("test authorization without login")
+    def test_authorization_without_login(self):
+        self.mainPage.open()
+        self.mainPage.click_enter_button()
+        self.mainPage.auth_enter_password(self.credentials.password)
+        self.mainPage.auth_click_enter_button()
+        self.mainPage.click_enter_button()
+        self.mainPage.get_incorrect_data_message_text()
+
+
+    @allure.title("test authorization with wrong password")
+    def test_authorization_with_wrong_password(self):
+        self.mainPage.open()
+        self.mainPage.click_enter_button()
+        self.mainPage.auth_enter_email(self.credentials.login)
+        self.mainPage.auth_enter_password(self.credentials.password+"test")
+        self.mainPage.auth_click_enter_button()
+        self.mainPage.click_enter_button()
+        self.mainPage.get_incorrect_data_message_text()
+
+
+    @allure.step("test authorization with password which's letters are in one register")
+    def test_authorization_with_password_in_same_register(self):
+        self.mainPage.open()
+        self.mainPage.click_enter_button()
+        self.mainPage.auth_enter_email(self.credentials.login)
+        self.mainPage.auth_enter_password(self.credentials.password)
+        self.mainPage.auth_click_enter_button()
+        self.mainPage.click_enter_button()
+        self.mainPage.get_incorrect_data_message_text()
+
+
+    @allure.step("test authorization with empty inputs")
+    def test_authorization_with_empty_inputs(self):
+        self.mainPage.open()
+        self.mainPage.click_enter_button()
+        self.mainPage.auth_click_enter_button()
+        self.mainPage.click_enter_button()
+        self.mainPage.get_incorrect_data_message_text()
