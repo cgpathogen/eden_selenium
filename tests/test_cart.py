5d38ff61ac38722f1d9e7ba446e747e4a5e33162
anatoly_bobrov <acrocanthbm@gmail.com>
2025-07-22 11:13:50 +0300

    Создан тест для тестирования удаления товаров из корзины

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -7,4 +7,19 @@ from base.base_test import BaseTest
 @allure.feature("Cart")
 class TestCart(BaseTest):
 
-    pass
+    @allure.title("test removing products from cart")
+    def test_removing_items_from_cart(self):
+        self.mainPage.open()
+        self.mainPage.click_choose_yes_button() # скрываем поп-ап с выбором города, который перекрывает кнопку каталога
+        self.mainPage.hover_catalogue_button()
+        self.mainPage.hover_link_in_catalogue_1st_lvl(3) # -> парфюмерия
+        self.mainPage.hover_link_in_catalogue_2st_lvl(2,True) # -> мужская парфюмерия
+        self.cataloguePage.filters.click_sort_options_dropdown()
+        self.cataloguePage.filters.click_sort_by_price_asc()
+        self.cataloguePage.filters.enter_min_price(1000)
+        self.cataloguePage.filters.enter_max_price(1100)
+        self.cataloguePage.filters.click_show_results_by_price_btn()
+        self.cataloguePage.add_goods_to_cart() # алгоритм добавления товаров в корзину
+        self.cataloguePage.click_cart_button()
+        self.cartPage.close_advertisement()
+        self.cartPage.remove_items_from_cart()
